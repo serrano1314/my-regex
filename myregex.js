882aba23aa7ce3549dae2fe1d8b22319885e3db0
Steven Serrano <steven.serrano@tup.edu.ph>
2021-05-03 21:01:09 +0800

    Create myregex.js

---
+++ myregex.js
@@ -0,0 +1,10 @@
+// REGEX
+
+// password validation
+// atleast 5chr with atleast 1number and without special characters
+let pwValid=/(?=[a-zA-Z0-9]{5})(?=[a-zA-Z0-9]*\d{1})/
+
+// remove whitespace at the star and at the end of the string
+" my stri ng".replace(/^\s(.*)\s$|^\s\1|\1\s$/,"$1")
+
+"     my stri ng     ".replace(/^\s+|\s+$/g,"")
