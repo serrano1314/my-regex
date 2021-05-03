b85c2d6e8acc6cb24b8d4fde4f049dad0c3ac206
Steven Serrano <steven.serrano@tup.edu.ph>
2021-05-03 21:01:47 +0800

    Update myregex.js

--- myregex.js
+++ myregex.js
@@ -5,6 +5,4 @@
 let pwValid=/(?=[a-zA-Z0-9]{5})(?=[a-zA-Z0-9]*\d{1})/
 
 // remove whitespace at the star and at the end of the string
-" my stri ng".replace(/^\s(.*)\s$|^\s\1|\1\s$/,"$1")
-
 "     my stri ng     ".replace(/^\s+|\s+$/g,"")
